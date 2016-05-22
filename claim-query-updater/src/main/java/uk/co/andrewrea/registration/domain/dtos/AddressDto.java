07a3513cff57249938cea9fee01379428e6f1dd4
Andrew Rea <email@andrewrea.co.uk>
2016-05-22 22:22:03 +0100

    Cleaning up an invalid directory structure

--- claim-query-updater/src/main/java/uk/co/andrewrea/registration/domain/dtos/AddressDto.java
+++
@@ -1,9 +0,0 @@
-package uk.co.andrewrea.registration.domain.dtos;
-
-public class AddressDto {
-    public String line1;
-    public String line2;
-    public String town;
-    public String city;
-    public String postCode;
-}
